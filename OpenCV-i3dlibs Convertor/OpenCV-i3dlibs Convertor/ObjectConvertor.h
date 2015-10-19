b2230221a5b4181ec5ef634f28c4003a9719ac2e
Mike <xvalent3@fi.muni.cz>
2015-10-19 21:18:04 +0200

    RGB16

--- OpenCV-i3dlibs Convertor/OpenCV-i3dlibs Convertor/ObjectConvertor.h
+++ OpenCV-i3dlibs Convertor/OpenCV-i3dlibs Convertor/ObjectConvertor.h
@@ -22,6 +22,7 @@ inline cv::Mat image3DToMat(const i3d::Image3d<i3d::RGB> img)
 	return cv::Mat(img.GetSizeX(), img.GetSizeY(), CV_8UC3, array).clone();
 }
 
+//TODO - otestovat
 inline cv::Mat image3DToMat(const i3d::Image3d<i3d::GRAY8> img)
 {
 	//Convertion data to uchar array
@@ -37,11 +38,12 @@ inline cv::Mat image3DToMat(const i3d::Image3d<i3d::GRAY8> img)
 	return cv::Mat(img.GetSizeX(), img.GetSizeY(), CV_8UC1, array).clone();
 }
 
+//TODO - otestovat
 inline cv::Mat image3DToMat(const i3d::Image3d<i3d::GRAY16> img)
 {
 	//Convertion data to ushort array
 	auto data = img.GetVoxelData();
-	auto array = new unsigned short(data.size());
+	auto array = new unsigned char(data.size());
 
 	for (size_t i = 0; i < data.size(); i++)
 	{
@@ -51,17 +53,152 @@ inline cv::Mat image3DToMat(const i3d::Image3d<i3d::GRAY16> img)
 	//Returning new Mat object with data from img
 	return cv::Mat(img.GetSizeX(), img.GetSizeY(), CV_16UC1, array).clone();
 }
-//TO DO
+
+//TODO - otestovat neni image
 inline cv::Mat image3DToMat(const i3d::Image3d<i3d::RGB16> img)
 {
 	//Convertion data to uchar*
 	auto data = img.GetVoxelData();
-	auto array = new unsigned char(data.size());
+	auto array = new unsigned short(data.size() * 3);
+
+
+	for (size_t i = 0; i < data.size(); i++)
+	{
+		array[i * 3] = data[i].red;
+		array[i * 3 + 1] = data[i].green;
+		array[i * 3 + 2] = data[i].blue;
+	}
 
 	//Returning new Mat object with data from img
 	return cv::Mat(img.GetSizeX(), img.GetSizeY(), CV_16UC3, array).clone();
 }
 
+inline i3d::Image3d<i3d::RGB>* MatToImage3D_RGB(const cv::Mat img)
+{
+	if (img.type() == CV_8UC3)
+	{
+		cv::Size s = img.size();
+		auto data = static_cast<unsigned char*>(img.data);
+		auto array = new std::vector<i3d::RGB>();
+
+		for (int i = 0; i < img.rows; i++)
+		{
+			for (int j = 0; j < img.cols; j++)
+			{
+				int b = data[img.step * i + j * 3];
+				int g = data[img.step * i + j * 3 + 1];
+				int r = data[img.step * i + j * 3 + 2];
+
+				int size = array->size();
+
+				array->push_back(i3d::RGB(r, g, b));
+			}
+		}
+
+		i3d::Image3d<i3d::RGB>* image = new i3d::Image3d<i3d::RGB>();
+		image->MakeRoom(s.width, s.height, 1);
+
+		for (size_t i = 0; i < s.area(); i++)
+		{
+			image->SetVoxel(i, array->at(i));
+		}
+
+		return image;
+	}
+	else
+	{
+		std::cout << "cv::Mat image type not handled in switch:" << img.type() << std::endl;
+		return nullptr;
+	}
+}
+
+
+inline i3d::Image3d<i3d::GRAY8>* MatToImage3D_GRAY8(const cv::Mat img)
+{
+	if (img.type() == CV_8UC1)
+	{
+		cv::Size s = img.size();
+		i3d::Image3d<i3d::GRAY8>* image = new i3d::Image3d<i3d::GRAY8>();
+		image->MakeRoom(s.width, s.height, 1);
+		auto data = static_cast<unsigned char*>(img.data);
+
+		for (size_t i = 0; i < s.area(); i++)
+		{
+			image->SetVoxel(i, data[i]);
+		}
+
+		return image;
+	}
+	else
+	{
+		std::cout << "cv::Mat image type not handled in switch:" << img.type() << std::endl;
+		return nullptr;
+	}
+}
+
+inline i3d::Image3d<i3d::GRAY16>* MatToImage3D_GRAY16(const cv::Mat img)
+{
+	if (img.type() == CV_16UC1)
+	{
+		cv::Size s = img.size();
+		i3d::Image3d<i3d::GRAY16>* image = new i3d::Image3d<i3d::GRAY16>();
+		image->MakeRoom(s.width, s.height, 1);
+		auto data = reinterpret_cast<unsigned short*>(img.data);
+
+		for (size_t i = 0; i < s.area(); i++)
+		{
+			image->SetVoxel(i, data[i]);
+		}
+
+		return image;
+	}
+	else
+	{
+		std::cout << "cv::Mat image type not handled in switch:" << img.type() << std::endl;
+		return nullptr;
+	}
+}
+//Zeptat se na RGB16
+inline i3d::Image3d<i3d::RGB16>* MatToImage3D_RGB16(const cv::Mat img)
+{
+	if (img.type() == CV_16UC3)
+	{
+		cv::Size s = img.size();
+		i3d::Image3d<i3d::RGB16>* image = new i3d::Image3d<i3d::RGB16>();
+		image->MakeRoom(s.width, s.height, 1);
+		auto data = reinterpret_cast<unsigned short*>(img.data);
+
+		auto array = new std::vector<i3d::RGB16>();
+
+		for (int i = 0; i < img.rows; i++)
+		{
+			for (int j = 0; j < img.cols; j++)
+			{
+				short b = data[img.step * i + j * 3];
+				short g = data[img.step * i + j * 3 + 1];
+				short r = data[img.step * i + j * 3 + 2];
+
+				int size = array->size();
+
+				array->push_back(i3d::RGB16(r, g, b));
+			}
+		}
+
+		for (size_t i = 0; i < s.area(); i++)
+		{
+			image->SetVoxel(i, data[i]);
+		}
+
+		return image;
+	}
+	else
+	{
+		std::cout << "cv::Mat image type not handled in switch:" << img.type() << std::endl;
+		return nullptr;
+	}
+}
+
+/*
 //TODO
 template<typename VOXEL>
 inline i3d::Image3d<VOXEL>* MatToImage3D(const cv::Mat img) 
@@ -119,4 +256,5 @@ struct conversion_proxy {
 	template<typename T> operator T() {
 		return MatToImage3D<T>(img);
 	}
-};
+};*/
+
--- OpenCV-i3dlibs Convertor/OpenCV-i3dlibs Convertor/main.cpp
+++ OpenCV-i3dlibs Convertor/OpenCV-i3dlibs Convertor/main.cpp
@@ -2,10 +2,10 @@
 #include <i3d/image3d.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "ObjectConvertor.h";
+#include "ObjectConvertor.h"
 
 using namespace cv;
-
+/*
 unsigned char* getUnsignedCharArray(i3d::Image3d<i3d::RGB> img)
 {
 	std::valarray<i3d::RGB> data = img.GetVoxelData();
@@ -27,49 +27,57 @@ Mat convertImage3DToMat(i3d::Image3d<i3d::RGB> img)
 	auto array = getUnsignedCharArray(img);
 
 	return 	Mat(img.GetSizeX(), img.GetSizeY(), CV_8UC3, array).clone();
-}
+}*/
 
 int main()
 {
-	i3d::Image3d<i3d::RGB16> img("Test-Picture-10x10.png");
+	//i3d::Image3d<i3d::RGB>* img = new i3d::Image3d<i3d::RGB>("Test-Picture-10x10.png");
 	Mat image;
-	image = imread("Test-Picture-10x10.png", IMREAD_COLOR); // Read the file
+	image = imread("Test-Picture-10x10-Red&Green.png", IMREAD_COLOR); // Read the file
 
-	i3d::Image3d<i3d::RGB>* img2(MatToImage3D<i3d::RGB>(image));
+	Mat O = Mat::eye(10, 10, CV_16UC3);
+	randu(O, Scalar::all(0), Scalar::all(65536));
+
+	//i3d::Image3d<i3d::RGB16>* img = new i3d::Image3d<i3d::RGB16>("RGB16picture.png");
+
+	//i3d::Image3d<i3d::RGB>* img2(MatToImage3D_RGB(image));
+	i3d::Image3d<i3d::RGB16>* img3(MatToImage3D_RGB16(O));
+	img3->SaveImage("Test-Picture-10x10_3.png");
 
 
-	Mat image2;
 
 
-	int a;
+	auto data = static_cast<unsigned char*>(image.data);
 
-	/*
-	for (int i = 0; i < 100; i++)
+	
+	/*for (int i = 0; i < 100; i++)
 	{
-		std::cout << i << ": " << static_cast<int>(input[i]) << std::endl;
+		std::cout << i << ": " << static_cast<int>(data[i]) << std::endl;
 	}
-	system("PAUSE");
+	system("PAUSE");*/
 	
-	
+	/*
 	int i, j, r, g, b;
 	for (int i = 0; i < image.rows; i++){
 		for (int j = 0; j < image.cols; j++){
-			b = input[image.step * i + j * 3];
-			g = input[image.step * i + j * 3 + 1];
-			r = input[image.step * i + j * 3 + 2];
+			b = data[image.step * i + j * 3];
+			g = data[image.step * i + j * 3 + 1];
+			r = data[image.step * i + j * 3 + 2];
 
 			std::cout << r << ", " << g << ", " << b << std::endl;
-			
+			std::cout << (int)img2->GetVoxelData()[i * image.rows + j].red << ", " << (int)img2->GetVoxelData()[i * image.rows + j].green << ", " << (int)img2->GetVoxelData()[i * image.rows + j].blue << std::endl;
+			std::cout << (int)img->GetVoxelData()[i * image.rows + j].red << ", " << (int)img->GetVoxelData()[i * image.rows + j].green << ", " << (int)img->GetVoxelData()[i * image.rows + j].blue << std::endl;
+
 		}
 		system("PAUSE");
 	}*/
 
 
 
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	/*namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
 	imshow("Display window", image2); // Show our image inside it.
 
-	waitKey(0); // Wait for a keystroke in the window
+	waitKey(0); // Wait for a keystroke in the window*/
 
 	return 0;
 }
